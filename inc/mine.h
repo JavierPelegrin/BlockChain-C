d686b6808e9a2dfd17a42e75c21b0c1972d0544d
JavierPelegrin <pelegring_j@lfmurcie.org>
2021-05-12 20:30:35 +0200

    update code

---
+++ inc/mine.h
@@ -0,0 +1,21 @@
+#ifndef __MINE_H__
+#define __MINE_H__
+
+#include <stdbool.h>
+#include "sha256_utils.h"
+#include "transaction.h"
+#include "BlockChain.h"
+
+void calculHash(char hash[SHA256_BLOCK_SIZE*2+1], char *infos);
+
+void createHash(Block **block);
+
+bool verifieHash(char *hash, int d);
+
+void mineBlock(Block **block, int Dificulty);
+
+// void testMerkleTreeFunction();
+
+char *calculmerkleRoot(Block *block);
+
+#endif
